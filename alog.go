8b357c46434dd077e46807cbe7d9f9fb37ef5207
Stefan Volkmer <stvolkme@microsoft.com>
2021-09-29 20:59:29 +0200

    Changes for Module3

--- alog.go
+++ alog.go
@@ -64,6 +64,7 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	al.m.Lock()
 	defer al.m.Unlock()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
